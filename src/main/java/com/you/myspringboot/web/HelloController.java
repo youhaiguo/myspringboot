95a2d62f1cf35e64456e8e419d699815fd11b08b
youhaiguo <1004556276@qq.com>
2019-06-13 17:43:04 +0800

    第一次提交

---
+++ src/main/java/com/you/myspringboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.you.myspringboot.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+}
